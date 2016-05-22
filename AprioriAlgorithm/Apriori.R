d503ad4942dcca06f99c7776ec40dc217eb8879a
SriviNerd <srividhya.karthik@gmail.com>
2016-05-22 12:20:35 +0530

    apriori rules mining inferences

--- AprioriAlgorithm/Apriori.R
+++ AprioriAlgorithm/Apriori.R
@@ -52,7 +52,7 @@ inspect(head(sort(itemSets,by="support"),20))
 #We get the minumum support threshold to 0.02 and do it for 3 product combination
 itemSets<-apriori(Transaction,parameter=list(minlen=1,maxlen=3,support=0.02,target="frequent itemsets"))
 summary(itemSets)
-inspect(head(sort(itemSets,by="support"),20))
+inspect(head(sort(itemSets,by="support"),50))
 #How to infer results 
 #Highersupport means higher% of occurance - Support(x) is count(x in the dataset)/Total counts of data set
 #Higher confience means the Rules are positively co-related but cannot gurantee -
@@ -62,4 +62,52 @@ inspect(head(sort(itemSets,by="support"),20))
 #lift < 1 means they are negatively co-related if items are statistically independant
 #Leverage also says if a rule is really related or a conincidence
 #leverage = support(x^y) - support(x)*support(y) if the value is > 0 there is usefulness to the rule and they are positively co-related
+inspect(head(sort(itemSets,by="support"),50))
 
+rules<-apriori(Transaction,parameter=list(minlen=1,maxlen=2,support=0.01,confidence = 0.01, target="rules"))
+inspect(head(sort(rules,by="lift"),10))
+
+rules<-apriori(Transaction,parameter=list(minlen=1,maxlen=3,support=0.01,confidence = 0.01, target="rules"))
+inspect(head(sort(rules,by="lift"),10))
+
+
+rules<-apriori(Transaction,parameter=list(minlen=1,maxlen=3,support=0.01,confidence = 0.01, target="rules"),appearance = list(lhs=c("VEGETABLES"),rhs=c("MILK"),default = "none"))
+inspect(head(sort(rules,by="lift"),10))
+
+rules<-apriori(Transaction,parameter=list(minlen=1,maxlen=4,support=0.001,confidence = 0.01, target="rules"))
+
+plot(rules)
+
+confidentRules <- rules[quality(rules)$confidence > 0.9]
+confidentRules
+
+highLiftRules <-head(sort(rules, by="lift"),5)
+plot(highLiftRules,method="graph",control=list(type="items"))
+
+#******class assignment
+data(Groceries)
+Groceries
+Groceries@data
+Groceries@itemInfo
+Groceries@itemInfo[10:20,]
+Groceries@data[,10:20]
+apply(Groceries@data[,1:10],2, function(r) paste(Groceries@itemInfo[r,"labels"],collapse=","))
+
+gitemSets<-apriori(Groceries,parameter=list(minlen=1,maxlen=1,support=0.02,target="frequent itemsets"))
+summary(gitemSets)
+inspect(head(sort(gitemSets,by="support"),20))
+
+gitemSets<-apriori(Groceries,parameter=list(minlen=1,maxlen=1,support=0.02,target="frequent itemsets"))
+summary(gitemSets)
+inspect(head(sort(gitemSets,by="support"),20))
+#you can mix and match
+grules<-apriori(Groceries,parameter=list(support=0.001,confidence = 0.6, target="rules"))
+inspect(head(sort(grules,by="lift"),20))
+plot(grules)
+
+
+confidentRules <- rules[quality(rules)$confidence > 0.9]
+confidentRules
+
+highLiftRules <-head(sort(grules, by="lift"),5)
+plot(highLiftRules,method="graph",control=list(type="items"))
